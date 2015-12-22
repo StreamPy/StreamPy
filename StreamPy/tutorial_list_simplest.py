02be80dcfdd4d4b51b6f037e522e6db4ab965bd0
Kanianthra Mani Chandy <kmchandy@Kanianthras-MBP.att.net>
2015-12-22 12:16:09 -0800

    Added stream agent example

--- StreamPy/tutorial_list_simplest.py
+++ StreamPy/tutorial_list_simplest.py
@@ -1,22 +1,31 @@
 from Stream import Stream
-from Operators import lf
+from Operators import stream_agent, lf
 from examples_element_wrapper import print_stream
 from random import randint
 
-def evens(x):
+# Returns even numbers in list xdef evens(x):
     return [v for v in x if v%2 == 0]
 
+# Returns multiplier times elements in list x
 def multiply(x, multiplier):
     return [v*multiplier for v in x]
 
+w = Stream('w')
 x = Stream('x')
 y = Stream('y')
 z = Stream('z')
+print_stream(w)
 print_stream(x)
 print_stream(y)
 print_stream(z)
 
-lf(x, y, evens)
-lf(x, z, multiply, multiplier=3)
+def double_list(lst): return [2*element for element in lst]
+stream_agent(inputs=x, outputs=z, f_type='list', f=double_list)
 
-x.extend([randint(0, 4) for _ in range(4)])
+# Using lf rather than stream_agent
+# y consists of even numbers in stream x
+lf(x, y, evens)
+# z consists of multiplier times elements in list x
+lf(x, w, multiply, multiplier=3)
+
+x.extend([randint(0, 5) for _ in range(4)])
