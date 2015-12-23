d719b2f8edc50d844d47891d468cf5a8e0c9a6a1
Kanianthra Mani Chandy <kmchandy@Kanianthras-MBP.att.net>
2015-12-22 21:19:15 -0800

    Added second example with step size greater than window size.

--- StreamPy/tutorial_window_simplest.py
+++ StreamPy/tutorial_window_simplest.py
@@ -10,14 +10,23 @@ from random import random
 x = Stream('x')
 y = Stream('y')
 z = Stream('z')
+
 print_stream(x)
 print_stream(y)
 print_stream(z)
 
 stream_agent(inputs=x, outputs=y, f_type='window', f=np.mean,
              window_size=5, step_size=5)
-
 # Alternative form
 wf(x, z, np.mean, window_size=5, step_size=5)
 
-x.extend([99.5+random() for _ in range(20)])
+x.extend([99.5+random() for _ in range(21)])
+
+# Second example
+# Illustrates that step_size may be larger than window_size.
+a = Stream('a')
+b = Stream('b')
+print_stream(a)
+print_stream(b)
+wf(a, b, np.mean, window_size=3, step_size=6)
+a.extend(range(20))
