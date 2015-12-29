81d6fc4f1e2509282d07913815fd95847a889491
Kanianthra Mani Chandy <kmchandy@Kanianthras-MBP.att.net>
2015-12-29 13:26:25 -0800

    Small changes

--- StreamPy/tutorial_window_simplest.py
+++ StreamPy/tutorial_window_simplest.py
@@ -18,15 +18,17 @@ print_stream(z)
 stream_agent(inputs=x, outputs=y, f_type='window', f=np.mean,
              window_size=5, step_size=5)
 # Alternative form
-wf(x, z, np.mean, window_size=5, step_size=5)
+wf(x, z, np.mean, 5, 5)
 
 x.extend([99.5+random() for _ in range(21)])
 
 # Second example
 # Illustrates that step_size may be larger than window_size.
+# Stream b samples every n-th element of stream a.
 a = Stream('a')
 b = Stream('b')
 print_stream(a)
 print_stream(b)
-wf(a, b, np.mean, window_size=3, step_size=6)
-a.extend(range(20))
+n = 6
+wf(a, b, lambda l: l[0], 1, n)
+a.extend(range(18))
