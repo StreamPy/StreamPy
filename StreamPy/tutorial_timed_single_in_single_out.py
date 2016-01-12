26c1942541367c210e9baf04dcd47c107c4bc0f3
Kanianthra Mani Chandy <kmchandy@Kanianthras-MBP.att.net>
2016-01-11 21:36:01 -0800

    Small changes

--- StreamPy/tutorial_timed_single_in_single_out.py
+++ StreamPy/tutorial_timed_single_in_single_out.py
@@ -9,17 +9,8 @@ print_stream(y)
 
 def h(window):
     print 'window', window
-    return [sum(t_and_v.value for t_and_v in window)]
-
-## timed_agent(f=h, inputs=[x], outputs=[y], state=None, call_streams=None,
-##                window_size=10, step_size=10)
-
-## stream_agent(inputs=x, outputs=y, f_type ='timed', f=h,
-##             state=None, call_streams=None,
-##             window_size=10, step_size=10)
+    return sum(v[1] for v in window)
 
 tf(inputs=x, outputs=y, func=h, window_size=10, step_size=10)
 
-x.extend([TimeAndValue(2, 10), TimeAndValue(6, 11),
-          TimeAndValue(12, 12), TimeAndValue(14, 13),
-          TimeAndValue(19, 12), TimeAndValue(22, 13)])
+x.extend([(2, 10), (6, 11), (12, 12), (14, 13), (19, 12), (32, 13)])
