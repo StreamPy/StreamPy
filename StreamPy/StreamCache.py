00c3e4668f8641a3e996594fde75ec8e54257738
Rahul Bachal <rbachal@caltech.edu>
2016-03-31 17:20:57 -0700

    Refactored cache to use ids for slices

--- StreamPy/StreamCache.py
+++ StreamPy/StreamCache.py
@@ -12,17 +12,33 @@ class StreamCache:
         self.max_slices = num_slices
         self.files = {}
         self.start = {}
+        self.slice_ids = range(0, num_slices)
         self.end = 0
         self.stream = stream
     
     def find_max_end(self):
         start = 0
-        end = 0
+        max_end = 0
         for key in self.start:
-            if self.start[key] > end:
+            end, slice_id = self.start[key]
+            if end > max_end:
                 start = key
-                end = self.start[key]
-        return start, end
+                max_end = end
+        return start, max_end
+
+    def find_min_start(self):
+        start = None
+        for key in self.start:
+            if start is None or key <= start:
+                start = key
+        return start
+
+    def find_min_start_except(self, start):
+        min_start = None
+        for key in self.start:
+            if key != start and (min_start is None or key < min_start):
+                min_start = key
+        return min_start
 
     def find_max_start_before(self, start):
         max_start = 0
@@ -46,7 +62,8 @@ class StreamCache:
             self.create_slice(end + 1, end + SECTOR_SIZE)
             start, end = end + 1, end + SECTOR_SIZE
             self.end = 0
-        self.recent_dict[(start, end)][self.end] = value
+        slice_id = self.start[start][1]
+        self.recent_dict[slice_id][self.end] = value
         self.end += 1
 
     def extend(self, values):
@@ -58,7 +75,8 @@ class StreamCache:
         while num_values != 0:
             num_indices = end - (self.end + start) + 1
             num_values_write = min(num_indices, num_values)
-            self.recent_dict[(start, end)][self.end:self.end + num_values_write] = values[values_start:values_start + num_values_write]
+            slice_id = self.start[start][1]
+            self.recent_dict[slice_id][self.end:self.end + num_values_write] = values[values_start:values_start + num_values_write]
             num_values -= num_values_write
             self.end += num_values_write
 
@@ -69,17 +87,17 @@ class StreamCache:
                 values_start += num_values_write
 
     def get_slice(self, start, end):
-        if (start, end) in self.recent_dict:
-            return (start, end, self.recent_dict[(start, end)])
+
         data = []
         f_start = self.find_max_start_before(start)
         cur_start = f_start
         while cur_start <= end:
             if cur_start in self.start:
-                f_end = self.start[cur_start]
-                data += self.recent_dict[(cur_start, f_end)]
-                del self.recent_dict[(cur_start, f_end)]
+                f_end, slice_id = self.start[cur_start]
+                data += self.recent_dict[slice_id]
+                del self.recent_dict[slice_id]
                 del self.start[cur_start]
+                self.slice_ids.append(slice_id)
                 if f_end >= end:
                     break
                 else:
@@ -92,9 +110,12 @@ class StreamCache:
                     break
                 else:
                    cur_start = f_end + 1 
-        
-        self.recent_dict[(f_start, f_end)] = data
-        self.start[f_start] = f_end
+        while len(self.recent_dict) >= self.max_slices:
+            self.evict_except(f_start)
+
+        slice_id = self.get_next_slice_id()
+        self.recent_dict[slice_id] = data
+        self.start[f_start] = f_end, slice_id
         self.num_slices = len(self.recent_dict)
         return (f_start, f_end, data)
 
@@ -102,26 +123,39 @@ class StreamCache:
         if self.num_slices == self.max_slices:
             self.evict()
         size = end - start + 1
-        self.recent_dict[(start, end)] = [0] * size
-        self.start[start] = end
+        slice_id = self.get_next_slice_id()
+        self.recent_dict[slice_id] = [0] * size
+        self.start[start] = end, slice_id
         self.num_slices += 1
         self.end = 0
-        return self.recent_dict[(start, end)]
+        return self.recent_dict[slice_id]
 
     def evict(self):
         begin = self.stream.get_begin()
-        for key in self.recent_dict:
-            start, end = key
-            if end < begin:
-                del self.recent_dict[key]
-                del self.start[start]
-                self.num_slices -= 1
-                return
-        slice_info = self.recent_dict.popitem()
-        start = slice_info[0][0]
-        del self.start[start]
-        self.write_to_file(slice_info)
+        min_start = self.find_min_start()
+        end, slice_id = self.start[min_start]
+        data = self.recent_dict[slice_id]
+        del self.recent_dict[slice_id]
+        del self.start[min_start]
+        self.slice_ids.append(slice_id)
         self.num_slices -= 1
+
+        if end >= begin:
+            self.write_to_file(((min_start, end), data))
+        return
+
+    def evict_except(self, start):
+        begin = self.stream.get_begin()
+        min_start = self.find_min_start_except(start)
+        end, slice_id = self.start[min_start]
+        data = self.recent_dict[slice_id]
+        del self.recent_dict[slice_id]
+        del self.start[min_start]
+        self.slice_ids.append(slice_id)
+        self.num_slices -= 1
+
+        if end >= begin:
+            self.write_to_file(((min_start, end), data))
         return
 
     def write_to_file(self, slice_info):
@@ -138,3 +172,9 @@ class StreamCache:
         os.remove(filename)
         del self.files[start]
         return data
+
+    def update_indices(self):
+        min_start = self.find_min_start()         
+
+    def get_next_slice_id(self):
+        return self.slice_ids.pop()
